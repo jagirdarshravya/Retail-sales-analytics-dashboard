315a51efbdca2538730c3ade630088d96f998b68
farha-k <farahaklm2005@gmail.com>
2026-03-16 13:00:35 +0530

    Update farha_analysis.sql

--- sql/farha_analysis.sql
+++ sql/farha_analysis.sql
@@ -1,7 +1,7 @@
 create database Brazilian_E_Commerce;
 use brazilian_E_Commerce;
-select count(*) from olist_customers;
-select count(distinct customer_unique_id)from olist_customers;
+select count(*) as Customer_count from olist_customers;
+select count(distinct customer_unique_id) as Total_customers from olist_customers;
 select customer_state,count(*) as total_customers from olist_customers group by customer_state order by total_customers desc;
 select customer_city,count(*) as total_customers from olist_customers group by customer_city order by total_customers desc limit 10;
 SELECT customer_state, COUNT(DISTINCT customer_city) AS cities_count FROM olist_customers GROUP BY customer_state ORDER BY cities_count DESC;
